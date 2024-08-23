fbd7fcfa74878cbbceee5c7100df826865abf3a6
Ritika Mehta <55449612+ritikamehta11@users.noreply.github.com>
2024-08-23 14:22:47 -0400

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,7 @@ const app = express();
 const PORT = process.env.PORT || 8888;
 app.use(cors(
   {
-    origin: ["https://ritika-mehta-portfolio.vercel.app"],
+    origin: ["https://portfolio-fe-wine.vercel.app"],
     methods: ["POST" , "GET"],
     credentials: true
   }
